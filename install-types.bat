b76617b7700aef0e20b5f292c739503ec812c977
AwfulLumos <awfullumos@gmail.com>
2026-03-31 15:20:01 +0800

    chore: remove unused batch scripts for installing types and removing lock files

--- install-types.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "c:\Users\Necrozma\MyWholeDataFolder\Projects\HealthWatchOlongapo"
-npm install --save-dev @types/react-dom
--- remove_lock.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if exist ".git\index.lock" (
-    del ".git\index.lock"
-    echo Lock file removed
-) else (
-    echo No lock file found
-)
-git status
